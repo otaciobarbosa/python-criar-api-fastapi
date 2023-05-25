10d53459fd90b8de335f7330a570b2088f98975e
Otácio Barbosa <otacio.barbosa@outlook.com>
2023-05-24 22:07:33 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+vendas = {
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
+    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5}
+}
+
+
+@app.get("/")
+def home():
+    return "API Python com FastAPI"
+
+@app.get("/vendas/{id_venda}")
+def pegar_venda(id_venda:int):
+    return vendas[id_venda]
